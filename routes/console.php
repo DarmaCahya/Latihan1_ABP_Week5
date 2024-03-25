0574e01f8a87b4a64d9efdf978266f21929a3024
Darma Cahya <95966668+DarmaCahya@users.noreply.github.com>
2024-03-25 23:37:54 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-Route::get('/lat1', 'App\Http\Controllers\Lat1Controller@index');
-
-Route::get('/lat1/m2', 'App\Http\Controllers\Lat1Controller@methode2');
